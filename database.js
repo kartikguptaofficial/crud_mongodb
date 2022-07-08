1778fb24d66090aa519145fc02ca823e9e9e395d
Kartik <kartikgupta962004@gmail.com>
2022-07-08 16:49:07 +0530

    first commit

---
+++ database.js
@@ -0,0 +1,16 @@
+const mongoose = require("mongoose");
+const path = require("path");
+require("dotenv").config({path: "./config.env"});
+
+mongoose.connect(process.env.DB)
+.then(() => console.log("DB connected"))
+.catch((err) => console.log(err))
+
+const dataSchema = new mongoose.Schema({
+    head: {type: String},
+    body: {type: String}
+})
+
+const Data = mongoose.model("data", dataSchema);
+
+ module.exports = Data
---
+++ index.js
@@ -0,0 +1,59 @@
+const express = require("express");
+const app = express();
+const path = require("path");
+const Data = require("./database")
+const bodyParser = require("body-parser");
+require("dotenv").config({path: "./config.env"});
+
+app.use(bodyParser({extended: true}))
+app.use(express.static(path.join(__dirname, "./public")))
+app.set("view engine", "ejs");
+
+app.get("/", async (req,res) => {
+    const getData = await Data.find({});
+    if(getData) {
+        res.render("index", {data:getData})
+    }
+})
+
+app.post('/post', async (req,res) => {
+    const { head, body } = req.body;
+    const add = new Data({head, body})
+    const save = await add.save()
+    if(save) {
+        // res.json({msg: "inserted!"})
+        res.redirect("/");
+    }
+})
+
+app.post("/edit/:id", async (req,res) => {
+    const id = req.params.id;
+    const { head, body } = req.body;
+    const edit = await Data.updateOne({_id: id}, {$set: {head, body}}) 
+    if(edit) {
+        // res.json("edited")
+        res.redirect(`/view/${id}`)
+    }
+})
+
+app.get("/delete/:id", async(req,res) => {
+    const id = req.params.id;
+    const delData = await Data.deleteOne({_id: id});
+    if(delData) {
+        res.redirect("/")
+    }
+})
+
+app.get("/view/:id", async (req,res) => {
+    const id = req.params.id;
+    const viewData = await Data.findOne({_id: id})
+    if(viewData) {
+        // res.json({viewData})
+        res.render("view", {note: viewData})
+    }
+})
+
+const PORT = process.env.PORT;
+app.listen(PORT, () => {
+    console.log(`Server started at port ${PORT}`)
+})
